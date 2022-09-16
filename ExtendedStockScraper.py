fddecaa804b8d4deb4a9b08f2047d213d232d1c7
André Muth <andre.muth@tutanota.com>
2022-09-16 19:58:08 +0200

    Update ExtendedStockScraper.py

--- ExtendedStockScraper.py
+++ ExtendedStockScraper.py
@@ -8,118 +8,108 @@ import re
 import time
 from datetime import datetime
 date = 'Date: ' + datetime.now().strftime('%d.%m.%Y, Time: %H:%M:%S')
-shortdate = datetime.now().strftime('%d.%m.%Y %H:%M')
 print(date)
 
 # At first I'm loading an Excel file with all the sources (information and links) about the stocks I want to investigate
 # and the data of my portfolio
 path_sources = 'C:\\Users\\andre\\Documents\\Python\Web_Scraper\Stocks_Sources.xlsx'
 sources = pd.read_excel(path_sources,sheet_name='Sources')
-# I want my Index to start at 1
-sources.index = sources.index + 1
-#print(sources)
 
-# creating lists for the variables I'm looking for
-names = []
-types = []
-prices = []
-plusminus = []
-percent = []
-factor = []
+# Preparation of the first DataFrame for general informations about the stock market
+stMaHeaders = ['name','type','price','CHG','CHG(%)','factor']
+dfStMarket = pd.DataFrame(columns = stMaHeaders)
 
-# preparing the data for the Dataframe "marketdata"
-for row in sources.iterrows():
-    time.sleep(.25)
-    r = requests.get(row[1]['link'])
+# This is a function that extracts data from my excel file and scrapes data from a finance webpage
+def stockMarketScraper(x):
+    time.sleep(.1)
+    r = requests.get(x[1]['link'])
     soup = BeautifulSoup(r.text, 'lxml')
-    # Scraping the current price and the intraday volatility with a for-loop for indices and currencies
+    name = x[1]['shortname']
+    category = x[1]['category']
+    price = soup.find('div',{'class':'col-xs-5'}).text.strip()
+    plusminus = soup.find('div',{'class':'col-xs-4'}).text.strip()
+    percent = soup.find('div',{'class':'col-xs-3'}).text.strip()
+    factor = re.sub('[USDEURCHFPKT.]','',price).replace(',','.')
+    datarow = [name,category,price,plusminus,percent,factor]
+    return datarow
+
+# I'm selecting and saving the stockmarket data I want to scrape:
+for row in sources.iterrows():
     if row[1]['category'] == 'Index' or row[1]['category'] == 'currency':
-        names.append(row[1]['name'])
-        types.append(row[1]['category'])
-        price = soup.find('div',{'class':'col-xs-5'}).text.strip()
-        prices.append(price)
-        plusminus.append(soup.find('div',{'class':'col-xs-4'}).text.strip())
-        percent.append(soup.find('div',{'class':'col-xs-3'}).text.strip())
-        factor.append(re.sub('[USDEURCHFPKT.]','',price).replace(',','.'))
+        dfStMarket.loc[len(dfStMarket)] = stockMarketScraper(row)
 
-# I'm creating the first Dataframe marketdata for an overview
-marketdata = pd.DataFrame({'name':names,
-                           'type':types,
-                           'price':prices,
-                           'CHG':plusminus,
-                           'CHG%':percent,
-                           'factor':factor})
-marketdata.index = marketdata.index + 1
-print(marketdata)
+# changing the start of the df index to 1
+dfStMarket.index = dfStMarket.index + 1
+print(dfStMarket)
 
-names = []
-types = []
-prices = []
-numbers = []
-b_course = []
-abase = []
-factor = []
 
-# filtering the Df and resetting the index
-sources_f = sources[sources['number'] > 0].reset_index()
+# Preparation of the dataframe Portfolio
+poHeaders = ['name','type','numbers','buying course','current price','asset base','current asset','win/loss','win/loss(%)']
+dfPortfolio = pd.DataFrame(columns = poHeaders)
+
+# Filtering of the sources-df (for stocks I'm invested in) and resetting the index
+sources_f = sources[sources['numbers'] > 0].reset_index()
 sources_f.index = sources_f.index +1
 
-# preparing the data for the Dataframe "portfolio"
-for row in sources_f.iterrows():
-    names.append(row[1]['name'])
-    types.append(row[1]['category'])
-    numbers.append(row[1]['number'])
-    b_course.append(row[1]['buyingcourse'])
-    abase.append(row[1]['asset base'])
+# Scraper function for my portfolio
+def portfolioStockScraper(x):
+    name = x[1]['shortname']
+    category = x[1]['category']
+    number = x[1]['numbers']
+    b_course = x[1]['buyingcourse']
+    abase = round(x[1]['asset base'],3)
     if row[1]['category'] == 'currency':
-        price = marketdata.loc[row[1]['index']]['price']
-    # Scraping the current price and the intraday volatility with a for-loop for shares and funds
+        price = dfStMarket.loc[x[1]['index']]['price']
     else:
-        time.sleep(.25)
-        r = requests.get(row[1]['link'])
+        # portfolio Scraper
+        time.sleep(.1)
+        r = requests.get(x[1]['link'])
         soup = BeautifulSoup(r.text, 'lxml')
         if row[1]['category'] == 'shares':
             price = soup.find('span', {'class': 'snapshot__value-current realtime-push'}).text.replace(' ','')
         elif row[1]['category'] == 'funds':
             price = soup.find('div', {'class':'table-responsive quotebox'}).find('td').text.replace(' ','')
     if 'USD' in price:
-        price = float(re.sub('[USDEURCHF.]','',price).replace(',','.')) /float(marketdata.loc[4]['factor'])
+        priceUSD = float(re.sub('[USDEURCHF.]', '',str(price)).replace(',','.'))
+        price = priceUSD/float(dfStMarket.loc[4]['factor'])
     else:
-        price = float(re.sub('[USDEURCHFPKT.]','',price).replace(',','.'))
-    price = str(round(price,4)) + 'EUR'
-    prices.append(price)
-    factor.append(float(re.sub('[USDEURCHFPKT]','',price)))
+        price = float(re.sub('[USDEURCHFPKT.]','',str(price)).replace(',','.'))
 
-# Based on the data I can calculate earnings, losses and my depot value
-cur_asset = [round(a*b,3) for a,b in zip(numbers,factor)]
-win_loss = [round(a-b,3) for a,b in zip(cur_asset,abase)]
-percent = [round((a-b)/b,3) for a,b in zip(cur_asset,abase)]
-asset_base = round(sum(abase),3)
-depot_value = round(sum(cur_asset),3)
-balance = depot_value-asset_base
-balance_p = (depot_value-asset_base)/asset_base
+    # Based on the data I can calculate my current asset, earnings and losses for every stock
+    cur_asset = round(number * price,2)
+    winloss = round(cur_asset - abase,2)
+    winlossP = round((cur_asset-abase)/abase*100,2)
 
-# Now I can create the second Dataframe "portfolio"
-portfolio = pd.DataFrame({'name':names,
-                           'type':types,
-                           'numbers':numbers,
-                           'buying course':b_course,
-                           'asset base':abase,
-                            shortdate:prices,
-                           'current asset':cur_asset,
-                           'win/loss':win_loss,
-                           'win/loss%':percent},
-                           index = [i for i in range(1,len(names)+1)])
+    # personal preference
+    if str(number)[-1] == '0':
+        number = int(number)
+    else:
+        number = str(round(number,5)).replace('.',',')
+    b_course = str(round(b_course,2)).replace('.',',')
+    tidyprice = str(round(price,2)).replace('.',',') + 'EUR'
 
-# Append the calculations to the Dataframe
-portfolio.loc[len(portfolio.index)+1] = ['total', 'all', '', '', asset_base, '', depot_value, balance, balance_p]
-print(portfolio)
+    datarow = [name,category,number,b_course,tidyprice,abase,cur_asset,winloss,winlossP]
+    return datarow
+
+# Saving scraped data into the dataframe Portfolio
+for row in sources_f.iterrows():
+    dfPortfolio.loc[len(dfPortfolio)] = portfolioStockScraper(row)
+dfPortfolio.index = dfPortfolio.index + 1
+#print(dfPortfolio)
+
+# values in total
+asset_base = round(sum(dfPortfolio['asset base']),2)
+depot_value = round(sum(dfPortfolio['current asset']),2)
+win_loss = round(sum(dfPortfolio['win/loss']),2)
+win_lossP = round(((depot_value-asset_base)/asset_base*100),2)
+
+# Append the calculations as a new row to the Dataframe
+dfPortfolio.loc[len(dfPortfolio.index)+1] = ['total','all','','','',asset_base,depot_value,win_loss,win_lossP]
+print(dfPortfolio)
 
 
-# Exporting and appending the Dataframes to an Excel file
+# Lastly I'm exporting the dataframes to an Excel file
 path = "C:\\Users\\andre\\OneDrive\Desktop\Stock_Scraper.xlsx"
 with pd.ExcelWriter(path, engine='openpyxl') as writer:
-    marketdata.to_excel(writer,sheet_name='Market')
-    portfolio.to_excel(writer,sheet_name='Portfolio')
-
-print('depot value: ' + str(depot_value))
+    dfStMarket.to_excel(writer,sheet_name='Market')
+    dfPortfolio.to_excel(writer,sheet_name='Portfolio')
